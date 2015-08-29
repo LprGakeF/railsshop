15bb93fb4cef95bdaf9e12e139cb43fa842d3ed4
LprGakeF <robert159c@yahoo.de>
2015-08-29 20:44:18 +0200

    dropdown categories is shown now only if any categories with any items exist. The entry of an category now only exists, if the category have actually any items. Adds some helpers for checking this cases

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,20 @@
 module CategoriesHelper
+
+  def has_items?(category)
+    return category.items.present?
+  end
+
+  def has_any_category_any_items?
+    if (Category.all.blank?)
+      return false
+    else
+      Category.all.each do |c|
+        if (c.items.present?)
+          return true
+        end
+      end
+      return false
+    end
+  end
+
 end
