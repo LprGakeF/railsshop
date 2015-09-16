bb5495d24e86e58713c6da81e72442e1ca30f506
LprGakeF <robert159c@yahoo.de>
2015-09-17 01:12:44 +0200

    adds some final changes

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,12 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => "587",
+  :domain               => "railsshoptest.com",
+  :user_name            => "railsshoptest@gmail.com",
+  :password             => "verysecureone",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
